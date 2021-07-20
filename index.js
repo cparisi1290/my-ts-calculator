4ffff641bad621cd371dc9af4a1d90a85b752f4a
Cassandra <cparisi1290@gmail.com>
2021-07-20 12:45:28 -0500

    create function to convert input strings to integer and verify inputs are valid numbers

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function main() {
     var firstStr = readline_sync_1.question('Enter first number:\n');
     var operator = readline_sync_1.question('Enter operator:\n');
     var secondStr = readline_sync_1.question('Enter second number:\n');
-    console.log(firstStr, operator, secondStr);
+    // verify inputs
+    var firstNum = isNumber(firstStr);
+    var secondNum = isNumber(secondStr);
+    console.log(firstNum, secondNum);
+}
+// to test if number inputs are numbers
+function isNumber(str) {
+    // convert string input to integer
+    var maybeNum = parseInt(str);
+    // check to see if input is a number
+    var isNum = !isNaN(maybeNum);
+    return isNum;
 }
 main();
--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
 import { question } from 'readline-sync';
 
-function main(): void
-{
+function main(): void {
     // ask for user inputs
     const firstStr: string =  question('Enter first number:\n')
     const operator: string =  question('Enter operator:\n')
     const secondStr: string =  question('Enter second number:\n')
-    console.log(firstStr, operator, secondStr)
+
+    // verify number inputs
+    const firstNum = isNumber(firstStr)
+    const secondNum = isNumber(secondStr)
+    console.log(firstNum, secondNum)
+}
+
+// to test if number inputs are numbers
+function isNumber(str: string): boolean {
+    // convert string input to integer
+    const maybeNum = parseInt(str);
+    // check to see if input is a number
+    const isNum: boolean = !isNaN(maybeNum);
+    return isNum;
 }
 
 main();
