78e1ee57417ff2409678975f6d441758129d005d
Cassandra <cparisi1290@gmail.com>
2021-07-20 12:11:32 -0500

    create user inputs for cli calculator

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 exports.__esModule = true;
 var readline_sync_1 = require("readline-sync");
 function main() {
-    console.log(readline_sync_1.question);
+    // ask for user inputs
+    var firstStr = readline_sync_1.question('Enter first number:\n');
+    var operator = readline_sync_1.question('Enter operator:\n');
+    var secondStr = readline_sync_1.question('Enter second number:\n');
+    console.log(firstStr, operator, secondStr);
 }
 main();
--- index.ts
+++ index.ts
@@ -2,7 +2,11 @@ import { question } from 'readline-sync';
 
 function main(): void
 {
-    console.log(question);
+    // ask for user inputs
+    const firstStr: string =  question('Enter first number:\n')
+    const operator: string =  question('Enter operator:\n')
+    const secondStr: string =  question('Enter second number:\n')
+    console.log(firstStr, operator, secondStr)
 }
 
 main();
